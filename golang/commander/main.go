48b765aebf9210357c3c2e3d71149888c95e8779
mason-fish <mason.e.fish@gmail.com>
2018-01-15 21:50:30 -0800

    add ignore

--- golang/commander/main.go
+++ golang/commander/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"os"
 
+	"fmt"
+
 	"github.com/mason-fish/public_projects/golang/githubTool"
 	"github.com/urfave/cli"
 )
 
 const (
-	reposFlag       = "repos"
 	accessTokenFlag = "access-token"
-	issuesFlag      = "issues"
+	userFlag        = "user"
 )
 
 func main() {
@@ -20,34 +21,78 @@ func main() {
 	cliApp.Commands = []cli.Command{
 		{
 			Name:      "github",
-			ShortName: "g",
+			ShortName: "gh",
 			Usage:     "initiate github operations",
 			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  reposFlag,
-					Usage: "access account repos",
-				},
-				cli.BoolFlag{
-					Name:  issuesFlag,
-					Usage: "access account issues",
-				},
 				cli.StringFlag{
 					Name:   accessTokenFlag,
 					Usage:  "supplies a github access token for authentication (default uses env GITHUB_ACCESS_TOKEN)",
 					EnvVar: "GITHUB_ACCESS_TOKEN",
 				},
+				cli.StringFlag{
+					Name:   userFlag,
+					Usage:  "provides the github account username (default uses env GITHUB_USER)",
+					EnvVar: "GITHUB_USER",
+				},
 			},
-			Action: func(c *cli.Context) error {
-				tool := ghtool.New(&ghtool.Config{
-					AuthToken: c.String(accessTokenFlag),
-				})
-				if c.Bool(reposFlag) {
-					return tool.PrintRepos()
-				}
-				if c.Bool(issuesFlag) {
-					return tool.PrintOpenIssues()
-				}
-				return nil
+			Subcommands: []cli.Command{
+				{
+					Name:      "repos",
+					ShortName: "r",
+					Usage:     "use repos api",
+					Subcommands: []cli.Command{
+						{
+							Name:      "create",
+							ShortName: "c",
+							Usage:     "create a new repository in the credential owner's account",
+							Action: func(c *cli.Context) error {
+								fmt.Println("feature is not yet available")
+								return nil
+							},
+						},
+						{
+							Name:      "list",
+							ShortName: "l",
+							Usage:     "list repositories associated with credential owner's account",
+							Action: func(c *cli.Context) error {
+								tool := ghtool.New(&ghtool.Config{
+									AuthToken: c.GlobalString(accessTokenFlag),
+								})
+								return tool.PrintRepos()
+							},
+						},
+					},
+				},
+				{
+					Name:      "issues",
+					ShortName: "i",
+					Usage:     "use issues api",
+					Subcommands: []cli.Command{
+						{
+							Name:      "create",
+							ShortName: "c",
+							Usage:     "create a new issue on a given repo",
+							Action: func(c *cli.Context) error {
+								tool := ghtool.New(&ghtool.Config{
+									AuthToken: c.GlobalString(accessTokenFlag),
+									User:      c.GlobalString(userFlag),
+								})
+								return tool.CreateIssue()
+							},
+						},
+						{
+							Name:      "list",
+							ShortName: "l",
+							Usage:     "list issues associated with credential owner's account",
+							Action: func(c *cli.Context) error {
+								tool := ghtool.New(&ghtool.Config{
+									AuthToken: c.GlobalString(accessTokenFlag),
+								})
+								return tool.PrintOpenIssues()
+							},
+						},
+					},
+				},
 			},
 		},
 	}
